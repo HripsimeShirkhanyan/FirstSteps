7ad533bbb6689119ee2f1d80f0f62c42c8757c63
Lenovo <Lenovo@DESKTOP-CBTL38B>
2019-01-07 18:51:29 +0400

    Add project files.

---
+++ HourAndMinuteAngles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HourAndMinuteAngles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindAngleInTime(2, 15);
+        }
+
+        public static void FindAngleInTime(int hours, int minutes)
+        {
+            double hourDegrees = (hours * 30) + (minutes * 30.0 / 60);
+            double minuteDegrees = minutes * 6;
+
+            double difference = Math.Abs(hourDegrees - minuteDegrees);
+
+            if (difference > 180)
+            {
+                difference = 360 - difference;
+            }
+
+            Console.WriteLine($"The angle between hour hand and minute hand is {difference} degrees");
+        }
+    }
+}
